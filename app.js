1ffe1e7af85dcc71d77db5ce7300d6bf7c179864
gokayoncu <gokayoncu35@gmail.com>
2025-03-21 15:42:07 +0300

    Update and Delete Mutations

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const typeDefs = `#graphql
     username: String!
     email: String!
   }
+  input UpdateUserInput {
+    username: String
+    email: String
+  }
 
   type Event {
     id: ID
@@ -43,6 +47,15 @@ const typeDefs = `#graphql
     location_id: ID!
     user_id: ID!
   }
+  input UpdateEventInput {
+    title: String
+    desc: String
+    date: String
+    from: String
+    to: String
+    location_id: ID
+    user_id: ID
+  }
 
   type Participant {
     id: ID
@@ -56,7 +69,10 @@ const typeDefs = `#graphql
     user_id: ID!
     event_id: ID!
   }
-
+  input UpdateParticipantInput {
+    user_id: ID
+    event_id: ID
+  }
   type Location {
     id: ID
     name: String
@@ -72,6 +88,16 @@ const typeDefs = `#graphql
     lat: Float!
     lng: Float!
   }
+  input UpdateLocationInput {
+    name: String
+    desc: String
+    lat: Float
+    lng: Float
+  }
+
+  type DeleteAllOutPut {
+    count: Int!
+  }
   type Query {
     users: [User!]!
     user(id: ID!): User  
@@ -83,10 +109,29 @@ const typeDefs = `#graphql
     location(id: ID!): Location 
   }
   type Mutation {
+    # Event
     createEvent(data: CreateEventInput!): Event!
+    updateEvent(id: ID!,data: UpdateEventInput!): Event!
+    deleteEvent(id: ID!): Event!
+    deleteAllEvent: DeleteAllOutPut!
+
+    # Participant
     createParticipant(data: CreateParticipantInput!): Participant!
+    updateParticipant(id: ID!,data: UpdateParticipantInput!): Participant!
+    deleteParticipant(id: ID!): Participant!
+    deleteAllParticipant: DeleteAllOutPut!
+
+    # Location
     createLocation(data: CreateLocationInput!): Location!
+    updateLocation(id: ID!,data: UpdateLocationInput!): Location!
+    deleteLocation(id: ID!): Location!
+    deleteAllLocation: DeleteAllOutPut!
+
+    # User
     createUser(data: CreateUserInput!): User!
+    updateUser(id: ID!,data: UpdateUserInput!): User!
+    deleteUser(id: ID!): User!
+    deleteAllUsers: DeleteAllOutPut!
   }
 `;
 
@@ -130,6 +175,7 @@ const resolvers = {
     },
   },
   Mutation: {
+    // Event
     createEvent: (parent, { data }) => {
       const newEvent = {
         id: nanoid(),
@@ -138,6 +184,34 @@ const resolvers = {
       events.push(newEvent);
       return newEvent;
     },
+    updateEvent:(parent,{data,id})=>{
+      const event = events.find((event) => String(event.id) === id);
+      if (!event) {
+        throw new Error(`Event with id ${id} not found`);
+      }
+      const updatedEvent = {
+        ...event,
+        ...data,
+      };
+      events.splice(events.indexOf(event), 1, updatedEvent);
+      return updatedEvent;
+    },
+    deleteEvent:(parent,{id})=>{
+      const eventIndex = events.findIndex((event) => String(event.id) === id);
+      if (eventIndex === -1) {
+        throw new Error(`Event with id ${id} not found`);
+      }
+      const deletedEvent = events[eventIndex];
+      events.splice(eventIndex, 1);
+      return deletedEvent;
+    },
+    deleteAllEvent:(parent,{})=>{
+      const eventsLength = events.length;
+      events.splice(0, eventsLength);
+      return { count: eventsLength };
+    },
+
+    // Participant
     createParticipant: (parent, { data }) => {
       const newParticipant = {
         id: nanoid(),
@@ -146,21 +220,104 @@ const resolvers = {
       participants.push(newParticipant);
       return newParticipant;
     },
+    updateParticipant: (parent, { data, id }) => {
+      const participant = participants.find((participant) => String(participant.id) === id);
+      if (!participant) {
+        throw new Error(`Participant with id ${id} not found`);
+      }
+      const updatedParticipant = {
+        ...participant,
+        ...data,
+      };
+      participants.splice(participants.indexOf(participant), 1, updatedParticipant);
+      return updatedParticipant;
+    },  
+    deleteParticipant: (parent, { id }) => {
+      const participantIndex = participants.findIndex((participant) => String(participant.id) === id);
+      if (participantIndex === -1) {
+        throw new Error(`Participant with id ${id} not found`);
+      }
+      const deletedParticipant = participants[participantIndex];
+      participants.splice(participantIndex, 1);
+      return deletedParticipant;
+    },
+    deleteAllParticipant:(parent,{})=>{
+      const participantsLength = participants.length;
+      participants.splice(0, participantsLength);
+      return { count: participantsLength };
+    },
+
+    // Location
     createLocation: (parent, { data }) => {
       const newLocation = {
         id: nanoid(),
-        ...data
+        ...data,
       };
       locations.push(newLocation);
       return newLocation;
     },
+    updateLocation:(parent,{data,id})=>{
+      const location = locations.find((location) => String(location.id) === id);
+      if (!location) {
+        throw new Error(`Location with id ${id} not found`);
+      }
+      const updatedLocation = {
+        ...location,
+        ...data,
+      };
+      locations.splice(locations.indexOf(location), 1, updatedLocation);
+      return updatedLocation;
+    },
+    deleteLocation:(parent,{id})=>{
+      const locationIndex = locations.findIndex((location)=>String(location.id)===id);
+      if(locationIndex == -1){
+        throw new Error ("Location is Not Found")
+      }
+      const location = locations[locationIndex];
+      locations.splice(locationIndex,1);
+      return location;
+    },
+    deleteAllLocation:(parent,{})=>{
+      const locationsLength = locations.length;
+      locations.splice(0, locationsLength);
+      return { count: locationsLength };
+    },
+
+    // User
     createUser: (parent, { data }) => {
       const newUser = {
         id: nanoid(),
-        ...data
+        ...data,
       };
       users.push(newUser);
       return newUser;
+    },
+    updateUser: (parent,{ data, id }) => {
+
+      const userIndex = users.findIndex((user) => String(user.id) === id);
+      if (userIndex === -1) {
+        throw new Error(`User with id ${id} not found`);
+      }
+      const updatedUser = {
+        ...users[userIndex],
+        ...data,
+      };
+      users.splice(userIndex, 1, updatedUser);
+      return updatedUser;
+    },
+    deleteUser:(parent,{id})=>{
+      const userIndex = users.findIndex((user)=>String(user.id)===id);
+      if(userIndex == -1){
+        throw new Error ("User is Not Found")
+      }
+      const user = users[userIndex];
+      users.splice(userIndex,1);
+      return user;
+    },
+    deleteAllUsers:(parent,{})=>{
+      const usersLength = users.length;
+      users.splice(0, usersLength);
+      return { count: usersLength };
     },
   },
   Event: {

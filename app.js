570a4355b195a9ee1b52fde4bf6cb331b322cd1f
gokayoncu <gokayoncu35@gmail.com>
2025-03-22 17:55:27 +0300

    Redis integration

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-import { ApolloServer } from "@apollo/server";
-import { PubSub } from 'graphql-subscriptions'; // PubSub importu
-import { readFile } from "fs/promises";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { nanoid } from "nanoid";
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+import { ApolloServer } from '@apollo/server';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import express from 'express';
+import { expressMiddleware } from '@apollo/server/express4';
+import { nanoid } from 'nanoid';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { readFile } from 'fs/promises';
+import pubSub from './pubSub.js';
 
-const pubSub = new PubSub(); // PubSub instance oluşturuluyor
+
 const jsonData = JSON.parse(await readFile(new URL("./data.json", import.meta.url)));
 const { users, events, participants, locations } = jsonData;
 
@@ -191,7 +198,7 @@ const resolvers = {
   },
   Subscription: {
     userCreated: {
-      subscribe: () => pubSub.asyncIterator('userCreated'),
+      subscribe: () => pubSub.asyncIterableIterator('userCreated'),
     },
     userUpdated: {
       subscribe: () => pubSub.asyncIterator('userUpdated'),
@@ -413,12 +420,45 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Create Express app
+const app = express();
+
+// Create HTTP server
+const httpServer = createServer(app);
+
+// Create WebSocket server
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+// Use WebSocket server with GraphQL schema
+useServer({ schema, context: () => ({ pubSub }) }, wsServer);
+
+// Create Apollo Server
+const apolloServer = new ApolloServer({
+  schema,
+  introspection: true,  // Enable introspection
+  playground: true      // Enable GraphQL Playground
 });
-console.log(`🚀 Server ready at: ${url}`);
+
+// Start Apollo Server
+await apolloServer.start();
+
+// Apply Apollo middleware to Express app
+app.use(
+  '/graphql',
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(apolloServer, {
+    context: async () => ({ pubSub }),
+  })
+);
+
+// Start HTTP server
+httpServer.listen(4000, () => {
+  console.log('🚀 HTTP Server ready at http://localhost:4000/graphql');
+  console.log('🚀 WebSocket Server ready at ws://localhost:4000/graphql');
+});
